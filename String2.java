d54026e1b6a63980b2055bdc5289044d289a9172
Purvi_Sharma <130075840+Purvi90@users.noreply.github.com>
2025-08-06 07:08:29 +0530

    Add files via upload

--- String2.java
+++ String2.java
@@ -1,16 +1,20 @@
 package j2;
 
 public class String2 {
+
+    // Reverse a string -- by use of strungBuilder---
+
     public static void main(String args[]) {
         StringBuilder sb = new StringBuilder("hello");
         
         for(int i=0;i<sb.length()/2;i++){
             int front = i;
-            int back = sb.length()-1-i;
+            int back = sb.length()-1-i; //5-1-0=>4
 
             char frontChar = sb.charAt(front);
             char backChar = sb.charAt(back);
 
+         // replace ke lie use hua hai -- setCharAt--
             sb.setCharAt(front, backChar);
             sb.setCharAt(back , frontChar);
         }
